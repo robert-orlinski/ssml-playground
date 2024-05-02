e4af6179553065de58f70363475e2eca001d285d
Robert Orliński <r.orlinski.contact@gmail.com>
2024-05-02 21:26:44 +0200

    say-as

--- google-cloud-text-to-speech/generate-one.js
+++ google-cloud-text-to-speech/generate-one.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const fsPromises = require("node:fs/promises");
 const client = new textToSpeech.TextToSpeechClient();
 
-const fileName = "";
+const fileName = "say-as";
 
 async function quickStart() {
   console.log("🔃 Processing:", fileName);
