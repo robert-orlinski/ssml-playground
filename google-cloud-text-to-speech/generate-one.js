b2e36232e48d7f115a2d64f3d19834468ba20c26
Robert Orliński <r.orlinski.contact@gmail.com>
2024-05-02 21:08:18 +0200

    divide scripts

---
+++ google-cloud-text-to-speech/generate-one.js
@@ -0,0 +1,28 @@
+const textToSpeech = require("@google-cloud/text-to-speech");
+
+const fs = require("fs");
+const fsPromises = require("node:fs/promises");
+const client = new textToSpeech.TextToSpeechClient();
+
+const fileName = [""];
+
+async function quickStart() {
+  console.log("🔃 Processing:", fileName);
+
+  const ssmlContent = fs.readFileSync(`./src/${fileName}.html`, "utf8");
+
+  const [response] = await client.synthesizeSpeech({
+    input: { ssml: ssmlContent },
+    voice: { languageCode: "en-US", ssmlGender: "NEUTRAL" },
+    audioConfig: { audioEncoding: "MP3" },
+  });
+
+  await fsPromises.writeFile(
+    `./output/${fileName}.mp3`,
+    response.audioContent,
+    "binary"
+  );
+  console.log("🎉 Processed:", fileName, "and finished!");
+}
+
+quickStart();
